2b95f31362fd52dbfdc14a0a1a86fa69c3940fd5
tlo <tlorusso@users.noreply.github.com>
2019-04-27 21:39:39 +0200

    hack for check

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+# suppress Notes
+# utils::suppressForeignCheck(c("group", "ID","INT"))
+
+utils::globalVariables(c("id", "resultat","res","geoid","langKey","bind_cols","kantonname","results2","geoLevelnummer","geoLevelname"))
