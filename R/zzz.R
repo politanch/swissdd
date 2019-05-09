89d6ca1400b1febbfd3b19413afd5146df73a445
tlorusso <th.lorusso@gmail.com>
2019-05-09 19:46:08 +0000

    startup message displaying data-source added

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,19 @@
-# suppress Notes
-# utils::suppressForeignCheck(c("group", "ID","INT"))
+# package startup-message (data source displayed)
+
+.onAttach <- function(lib, pkg)
+{
+  packageStartupMessage('**************************************************************')
+  packageStartupMessage('*                          swissdd 0.0.9                     *')
+  packageStartupMessage('*                     provided by politan.ch                 *')
+  packageStartupMessage('*                                                            *')
+  packageStartupMessage('*                                                            *')
+  packageStartupMessage('*           Data source: Federal Statistical Office          *')
+  packageStartupMessage('*                   https://www.bfs.admin.ch/                *')
+  packageStartupMessage('*                                                            *')
+  packageStartupMessage('**************************************************************')
+}
+
+# suppress notes
 
 utils::globalVariables(c("id", "resultat",
                          "res","geoid","langKey",
@@ -7,3 +21,6 @@ utils::globalVariables(c("id", "resultat",
                          "results2","geoLevelnummer",
                          "geoname","district_id",
                          "geoLevelname","text","ktid"))
+
+
+
