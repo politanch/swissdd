2f454d9db254b0fc2c3d9267f3e3fdb3a6b6b49c
tlorusso <th.lorusso@gmail.com>
2019-08-01 06:38:31 +0000

    tests

---
+++ tests/testthat/test_availabledates.R
@@ -0,0 +1,13 @@
+context("available votedates")
+
+test_that("available votedates works", {
+  
+  swissdates <- available_votedates()
+  
+  expect_is(swissdates, "Date")
+  
+  cantdates <- available_votedates(geolevel = "canton")
+  
+  expect_is(cantdates, "Date")
+  
+})
