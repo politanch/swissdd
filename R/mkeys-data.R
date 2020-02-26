967f921aded03395909842e812063500621c9241
Thomas <willi@uega.ch>
2020-02-26 14:13:36 +0100

    added merge keys data and description file

---
+++ R/mkeys-data.R
@@ -0,0 +1,18 @@
+#' Merge data
+#'
+#' Merge keys that allow to easily combine aggregate data from 
+#' swissvotes with post-ballot surveys (VOX).
+#'
+#' @docType data
+#'
+#' @usage data(mkeys)
+#'
+#' @keywords datasets
+#'
+#' @source Walder, Maxime ([Twitter](https://twitter.com/WalderMaxime))
+#'
+#' @examples
+#' data(mkeys)
+#' bsnr <- mkeys$bfsnr
+#' voxnr <- mkeys$projetx
+"mkeys"
