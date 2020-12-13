d52309bd4034115a9dce6610cc50023ef660c71c
tlorusso <th.lorusso@gmail.com>
2020-12-13 18:59:09 +0100

    startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 .onAttach <- function(lib, pkg)
 {
   packageStartupMessage('************************************************************')
-  packageStartupMessage('*                       swissdd 1.0.4                      *')
+  packageStartupMessage('*                     swissdd 1.0.4.9999                   *')
   packageStartupMessage('*                  developed by politan.ch                 *')
   packageStartupMessage('*                                                          *')
   packageStartupMessage('*                     Data sources:                        *')
